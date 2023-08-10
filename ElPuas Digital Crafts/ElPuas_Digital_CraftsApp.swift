23fe048da51df31253fa6c73c28074069a9f874f
Alfredo Navas <elpuas@gmail.com>
2023-08-10 08:58:03 -0600

    Initial Commit

---
+++ ElPuas Digital Crafts/ElPuas_Digital_CraftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElPuas_Digital_CraftsApp.swift
+//  ElPuas Digital Crafts
+//
+//  Created by Carlo  Navas on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ElPuas_Digital_CraftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElPuas Digital CraftsUITests/ElPuas_Digital_CraftsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ElPuas_Digital_CraftsUITestsLaunchTests.swift
+//  ElPuas Digital CraftsUITests
+//
+//  Created by Carlo  Navas on 10/8/23.
+//
+
+import XCTest
+
+final class ElPuas_Digital_CraftsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
